ba19a930f5b03355aba166d1ce28e6e33ec0fae3
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-02 17:20:28 +0200

    Implement email validation with promises
    Added email validation function with promise handling.

--- lab1.js
+++ lab1.js
@@ -1,17 +1,27 @@
-const p = new Promise((resolve) => {
-    setTimeout(() => resolve("Done"), 1000);
-});
-
-p.then(result => console.log(result)); 
-
-function doSomething(callback) {
-  callback();
+//Завдання №1
+console.log("Завдання №1\n");
+function validateEmail(email) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (checkEmail(email)) {
+                resolve(email); 
+            } else {
+                reject("Помилка: некоректний email"); 
+            }
+        }, 300);
+    });
 }
 
-let value = 1;
+const checkEmail = (email) => {
+    return email.includes("@") && email.includes(".");
+};
 
-doSomething(() => {
-  value = 2;
-});
+validateEmail("test@me.com")
+    .then(data => console.log("Валідний:", data))
+    .catch(error => console.error(error));
 
-console.log(value); 
+validateEmail("invalid-email")
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+//Завдання №2
