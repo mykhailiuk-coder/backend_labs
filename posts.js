00b62fe27a2e54e3bf7571851bdc2dfa9e82b984
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-11 08:32:12 +0200

    Implement data fetching from API in posts.js
    Fetch data from the API and handle errors.

--- posts.js
+++ posts.js
@@ -3,4 +3,18 @@
 // This function should print in console array of obects with the following structure {id, title, user_id}
 // and handle possible errors 
 
-// put your code here
+async function getData() {
+  const url = "https://gorest.co.in/public/v2/posts";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`); 
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error("Error fetching data:", error.message); 
+  }
+}
+
+getData();
